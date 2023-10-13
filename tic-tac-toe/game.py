1931b6821ca674565e99ffb5a674f7419ccc6339
anilkusc <anilkuscu95@gmail.com>
2023-10-14 01:34:28 +0300

    feat: :art: game improvements

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -2,9 +2,45 @@ class Game:
   def __init__(self, player1, player2):
     self.player1 = player1
     self.player2 = player2
+    
+    self.turn = 1
     self.game_board = [[0,0,0],[0,0,0],[0,0,0]]
   
-  def render_boards(self):
+  def start(self):
+    self.move = 1
+    while True:
+      print("who's turn:"+ str(self.turn))
+      print("move:"+ str(self.move))
+      self.render_board()
+      x = int(input("please enter x coordinate: "))
+      y = int(input("please enter y coordinate: "))
+      while not self.is_move_valid(x,y):
+        print("invalid move. please try again.")
+        x = int(input("please enter x coordinate: "))
+        y = int(input("please enter y coordinate: "))
+      self.game_board[y][x] = self.turn
+      if self.is_winner():
+        print("winner is player: " + str(self.turn))
+        self.render_board()
+        self.restart_game()
+        continue
+      if self.is_draw():
+        print("this is a draw!")
+        self.render_board()
+      if self.turn == 1:
+        self.turn = 2
+      else:
+        self.turn = 1
+      self.move += 1
+
+  def restart_game(self):
+    print("game is restarting...")
+    self.turn = 1
+    self.game_board = [[0,0,0],[0,0,0],[0,0,0]]
+    self.move = 1
+    print("#####################################")
+
+  def render_board(self):
     print("_____________")
     print("| "+self.render_board_item(self.game_board[0][0])+" | "+self.render_board_item(self.game_board[0][1])+" | "+self.render_board_item(self.game_board[0][2])+" |")
     print("| "+self.render_board_item(self.game_board[1][0])+" | "+self.render_board_item(self.game_board[1][1])+" | "+self.render_board_item(self.game_board[1][2])+" |")
@@ -20,8 +56,30 @@ class Game:
       return "O"
 
   def is_move_valid(self,x,y):
-    if 0 > x or 3 < x or 0 > y or 3 < y: 
+    if 0 > x or 2 < x or 0 > y or 2 < y: 
       return False
     if self.game_board[x][y] != 0:
       return False    
+    return True
+
+  def is_winner(self):
+    for row in self.game_board:
+      # control rows
+      if row[0] == row[1] == row[2] and row[0] > 0 and row[1] > 0 and row[2] > 0:
+        return True
+    for i,_ in enumerate(self.game_board):
+      # control columns  y,x
+      if self.game_board[0][i] == self.game_board[1][i] == self.game_board[2][i] and self.game_board[0][i] > 0 and self.game_board[1][i] > 0 and self.game_board[2][i] > 0:
+        return True
+    if self.game_board[0][0] == self.game_board[1][1] == self.game_board[2][2] and self.game_board[0][0] > 0 and self.game_board[1][1] > 0 and self.game_board[2][2] > 0:
+      return True
+    if self.game_board[0][2] == self.game_board[1][1] == self.game_board[2][0] and self.game_board[0][2] > 0 and self.game_board[1][1] > 0 and self.game_board[2][0] > 0:
+      return True
+    return False
+
+  def is_draw(self):
+    for row in self.game_board:
+      for item in row:
+        if item == 0:
+          return False    
     return True
--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,3 +1,3 @@
 from game import Game
 game = Game("test","test")
-game.render_boards()
+game.start()
