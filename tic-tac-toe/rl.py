86da872c6406d2c88222226f7c5b029f7c09121f
anilkusc <anilkuscu95@gmail.com>
2023-10-15 16:18:04 +0300

    fix: :bug: fix for states

--- tic-tac-toe/rl.py
+++ tic-tac-toe/rl.py
@@ -9,19 +9,18 @@ class RL:
         self.action_space_size = 9
         self.state_space_size = len(self.all_possible_states)
         self.q_table = np.zeros((self.state_space_size, self.action_space_size))
-        self.game_board = [[0,0,0],[0,0,0],[0,0,0]]
 
-        self.win_reward = 2
+        self.win_reward = 20
         self.draw_reward = 1
-        self.lost_reward = 0
+        self.lost_reward = -10
         self.num_episodes = 100
         self.max_steps = 9
-        self.learning_rate = 0.5
+        self.learning_rate = 0.7
         self.discount_rate = 0.99
         self.epsilon = 1
         self.max_epsilon = 1
         self.min_epsilon = 0.0001
-        self.exploration_decay_rate = 0.0001
+        self.exploration_decay_rate = 0.01
 
     def move(self,current_table):
         for i,t in enumerate(self.all_possible_states):
@@ -57,11 +56,12 @@ class RL:
 
     def train(self):
         for episode in range(self.num_episodes):
+            print("%",episode*100/self.num_episodes)
             state = 0
             done = False
             for step in range(self.max_steps):
                 exp_tradeoff = random.uniform(0, 1)
-                possible_actions = self.find_possible_actions()
+                possible_actions = self.find_possible_actions(self.convert_state_to_array(self.all_possible_states[state]))
                 if exp_tradeoff > self.epsilon:
                     # exploitation
                     action = np.argmax(self.q_table[state, :])
@@ -75,14 +75,13 @@ class RL:
                     break
             self.epsilon = self.min_epsilon + (self.max_epsilon - self.min_epsilon) * np.exp(-self.exploration_decay_rate*episode)
     
-    def find_possible_actions(self):
+    def find_possible_actions(self,state_array):
         possible_actions = []
-        state_array = self.convert_state_to_array(self.game_board)
         for i,s in enumerate(state_array):
             if s == 0:
                 possible_actions.append(i)
         if len(possible_actions) == 0:
-            possible_actions.append[-1]
+            possible_actions.append(-1)
         return possible_actions
 
     def create_states(self):
@@ -102,12 +101,20 @@ class RL:
             return self.win_reward,True
         if self.is_draw(state):
             return self.draw_reward,True
-        return self.lost_reward,False
+        if self.is_lost(state):
+            return self.lost_reward,True
+        return 0,False
 
     def step(self,current_state,action):
         current_table = self.all_possible_states[current_state]
         next_state = self.convert_state_to_array(current_table)
         next_state[action] = 1
+        # Player 2 plays randomly
+        possible_actions = self.find_possible_actions(next_state)
+        if len(possible_actions) == 1 and possible_actions[0] == -1:
+            pass
+        else:
+            next_state[random.choice(possible_actions)] = 2
         for i,state in enumerate(self.all_possible_states):
             if self.convert_state_to_array(state) == next_state:
                 new_state = i
@@ -126,24 +133,30 @@ class RL:
     def convert_array_to_state(self,array):
         return [[array[0],array[1],array[2]],[array[3],array[4],array[5]],[array[6],array[7],array[8]]]
     
-    def is_winner(self,action):
-        board = self.convert_array_to_state(action)
-        for row in board:
-            if row[0] == row[1] == row[2] == 1:
-                return True
-        for i,_ in enumerate(self.game_board):
-            if self.game_board[0][i] == self.game_board[1][i] == self.game_board[2][i] == 1:
-                return True
-            if self.game_board[0][0] == self.game_board[1][1] == self.game_board[2][2] == 1:
-                return True
-            if self.game_board[0][2] == self.game_board[1][1] == self.game_board[2][0] == 1:
-                return True
+    def is_winner(self,state):
+        if state[0] == state[1] == state[2] == 1 or state[3] == state[4] == state[5] == 1 or state[6] == state[7] == state[8] == 1:
+            return True
+        if state[0] == state[3] == state[6] == 1 or state[1] == state[4] == state[7] == 1 or state[2] == state[5] == state[8] == 1:
+            return True
+        if state[0] == state[3] == state[6] == 1 or state[1] == state[4] == state[7] == 1 or state[2] == state[5] == state[8] == 1:
+            return True
+        if state[0] == state[4] == state[8] == 1 or state[2] == state[4] == state[6] == 1:
+            return True
         return False        
 
-    def is_draw(self,action):
-      board = self.convert_array_to_state(action)
-      for row in board:
-        for item in row:
+    def is_lost(self,state):
+        if state[0] == state[1] == state[2] == 2 or state[3] == state[4] == state[5] == 2 or state[6] == state[7] == state[8] == 2:
+            return True
+        if state[0] == state[3] == state[6] == 2 or state[1] == state[4] == state[7] == 2 or state[2] == state[5] == state[8] == 2:
+            return True
+        if state[0] == state[3] == state[6] == 2 or state[1] == state[4] == state[7] == 2 or state[2] == state[5] == state[8] == 2:
+            return True
+        if state[0] == state[4] == state[8] == 2 or state[2] == state[4] == state[6] == 2:
+            return True
+        return False        
+
+    def is_draw(self,state):
+        for item in state:
           if item == 0:
             return False
-      return True
+        return True
