ee4c954a939e5abf36ab8dabeb911bcefddfb341
anilkusc <anilkuscu95@gmail.com>
2023-11-15 14:32:07 +0300

    fix: :fire: removed padding

--- cifar10/main.py
+++ cifar10/main.py
@@ -12,9 +12,9 @@ class Net(nn.Module):
         super(Net, self).__init__()
         # convolutional layer
         ##in,out,kernel size,padding
-        self.conv1 = nn.Conv2d(3, 32, 3,padding=1)
-        self.conv2 = nn.Conv2d(32, 64, 3,padding=1)
-        self.conv3 = nn.Conv2d(64, 128, 3,padding=1)
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
         # pool layer
         self.pool = nn.MaxPool2d(2, 2)
         # fully connected layer
@@ -33,6 +33,7 @@ class Net(nn.Module):
         x = self.dropout1(x)
         x = self.pool(F.relu(self.conv3(x)))
         x = self.dropout1(x)
+        # flattening(prepare image data for fully connected data input)
         x = x.view(-1, 128 * 2 * 2)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
